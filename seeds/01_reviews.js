38ff9892a5111b2c7625b072f1f0161a18e2e4dd
Brumz <bmanrumsey24@gmail.com>
2018-12-07 14:22:01 -0700

    fix

--- seeds/01_reviews.js
+++ seeds/01_reviews.js
@@ -4,7 +4,8 @@ exports.seed = function (knex, Promise) {
       return knex('reviews').insert([{
         title: "This sucks",
         text: "Taste like dogshit",
-        rating: 2
+        rating: 2,
+        snack_id: 1
       }]);
     });
 };
